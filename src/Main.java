57570d1c8760338988dc75db675ca385e1e324e3
Oguzhan Cataltas <cataltasoguzhan@gmail.com>
2023-01-07 19:44:42 +0300

    first

--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,21 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        int age, mod;
+        int age;
 
         System.out.print("Yıl Giriniz : ");
         age = input.nextInt();
 
-        mod = age % 4;
-
-        if (mod == 0) {
-            System.out.println(age + " bir artık yıldır");
-        }else {
-            System.out.println(age + " bir artık yıl değildir!!!");
+        if (age % 100 == 0) {
+            if (age % 400 == 0) {
+                System.out.print(age + " artık bir yıldır!");
+            } else {
+                System.out.print(age + " artık bir yıl değildir.");
+            }
+        } else if (age % 4 == 0) {
+            System.out.print(age + " artık bir yıldır!");
+        } else {
+            System.out.print(age + " artık bir yıl değildir.");
         }
     }
 }
